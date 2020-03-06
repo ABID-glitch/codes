20e53c78be0b2ddc638b874eb7740a4dfa3fd46e
ABID-glitch <61841766+ABID-glitch@users.noreply.github.com>
2020-03-06 23:02:35 +0530

    Create constructor1.cpp

---
+++ constructor1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class complex{
+private:
+int a,b;
+public:
+complex(){
+cout<<"Hello constructor"<<endl;
+}
+};
+int main(){
+complex c1,c2,c3,c4;
+return 0;
+}
