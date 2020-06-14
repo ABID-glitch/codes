bdcb9adc8f1d0a07c59c5e63cefc5dcb96e4f081
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-15 00:35:41 +0530

    Create Truthtable.cpp

---
+++ Experiment1/Truthtable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"X|Y|Z|X*Y|X*Y+Z|"<<endl;
+for(int x=0;x<2;x++){
+    for(int y=0;y<2;y++){
+            for(int z=0;z<2;z++){
+        int a=x*y+z;
+        if(a>1)
+            a=1;
+            cout<<x<<"|"<<y<<"|"<<z<<"|"<<x*y<<"|"<<a;
+            cout<<"\n";
+            }
+    }
+}
+return 0;
+}
+
