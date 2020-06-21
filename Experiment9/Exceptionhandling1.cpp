30150fef40239890b0a2047e1aa3298b0e9a82ba
Abid Hussain <61841766+ABID-glitch@users.noreply.github.com>
2020-06-21 19:33:47 +0530

    Update Exceptionhandling1.cpp

--- Experiment9/Exceptionhandling1.cpp
+++ Experiment9/Exceptionhandling1.cpp
@@ -3,27 +3,28 @@
 using namespace std;
 
 void check(int x){
-           try
-           {
-            cout<<"Check if there is any exception"<<endl;
-            if(x<0){
-                cout<<"Throw exception"<<endl;
-                throw x;
-            }
-            cout<<"No exception found"<<endl;
-           }
-           catch(int){
-           cout<<"Exception is caught!, Integer is negative"<<endl;
-           abort();}
-           }
-           void calculate(int x){
-           check(x);
-           cout<<"Square root of "<<x<<"is"<<sqrt(x)<<endl;
-           }
+try{
+cout<<"Check if there is any exception"<<endl;
+if(x<0){
+cout<<"Throw exception"<<endl;
+throw x;
+}
+cout<<"No exception found"<<endl;
+}
+
+catch(int){
+cout<<"Exception found!, Integer is negative"<<endl;
+abort();
+}
+}
+void calculate(int x){
+check(x);
+cout<<"Square root of"<<x<<" is "<<sqrt(x)<<endl;
+}
+
 int main(){
-    int x;
-cout<<"Enter an integer you need the root of "<<endl;
+int x;
+cout<<"Enter the integer you want root of "<<endl;
 cin>>x;
 calculate(x);
 }
-
